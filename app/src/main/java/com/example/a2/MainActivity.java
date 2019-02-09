d3b337bce849a147f4719f03af26539a5265b61e
mariah07 <mariahpinto2@gmail.com>
2019-02-09 18:33:30 -0400

    ok

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -12,24 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText password = (EditText) findViewById(R.id.editText);
-                EditText text = (EditText) findViewById(R.id.editText2);
-
-                Validator validator = new Validator();
-
-                String word = password.getText().toString();
-
-                if(validator.Validate(word) == 5)
-                    text.setText("Strong enough");
-                else
-                    text.setText("Not Strong Enough");
-            }
-        });
-
+        
     }
 }
