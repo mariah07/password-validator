8037496739029ad000e2c7726a6157204eb2f2ed
mariah07 <mariahpinto2@gmail.com>
2019-02-09 18:40:46 -0400

    ok

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.a2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText password = (EditText) findViewById(R.id.editText);
+                EditText text = (EditText) findViewById(R.id.editText2);
+
+                Validator validator = new Validator();
+
+                String word = password.getText().toString();
+
+                if(validator.Validate(word) == 5)
+                    text.setText("Strong enough");
+                else
+                    text.setText("Not Strong Enough");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/a2/Validator.java
+++ app/src/main/java/com/example/a2/Validator.java
@@ -3,12 +3,19 @@ package com.example.a2;
 
 public class Validator {
 
+    public Validator() {}
 
     public int Validate(String password){
         int i = 0;
         if(password.length() > 7)
             i++;
         if(!password.equalsIgnoreCase("password") )
+            i++;
+        if(password.matches(".*\\d.*"))
+            i++;
+        if(!password.matches("[a-zA-Z0-9]*"))
+            i++;
+        if(password.matches(".*[A-Z].*"))
             i++;
 
         return i;
